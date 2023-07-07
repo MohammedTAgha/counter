c9393990af9a9d9c70d1e551e2b54d7d3596c119
MOHAMMED T AGHA <mohammedtabsemagha@gmail.com>
2023-07-07 22:42:42 +0300

    formt

--- src/App.js
+++ src/App.js
@@ -1,49 +1,42 @@
-
- 
-import React , { Component } from 'react';
+import React, { Component } from "react";
 import styled from "styled-components";
-import { Counter } from './Components';
+import { Counter } from "./Components";
 
 const Container = styled.div`
-        position: absolute;
-  
+  position: absolute;
+
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
-    height: 100%;
-    `;
-    class App extends Component {
+  height: 100%;
+`;
+class App extends Component {
   state = {
-    total : 0
+    total: 0,
+  };
+
+  componentDidMount() {
+    this.setState({ total: 0 });
   }
 
- componentDidMount(){
-  this.setState({total:0})
- }
- 
-  
-  render(){
-    const getTotal=(num)=>{
-      console.log("exixude getTotal " , num)
+  render() {
+    const getTotal = (num) => {
+      console.log("exixude getTotal ", num);
       this.setState((prevState) => ({
         total: prevState.total + num,
-      })
-      
-      )
-    }
-     return (
-    <Container>
+      }));
+    };
+    return (
+      <Container>
         <Counter initial={2} getTotal={getTotal} />
-        <Counter initial={4} getTotal={getTotal}/>
-        <Counter initial={1} getTotal={getTotal}/>
+        <Counter initial={4} getTotal={getTotal} />
+        <Counter initial={1} getTotal={getTotal} />
         <p>total : {this.state.total}</p>
-    </Container>
-  );
+      </Container>
+    );
   }
 }
 
 export default App;
-
-
